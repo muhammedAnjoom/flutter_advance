951a49f75e26f2af5d5615d9484fd4427e41ca64
muhammedAnjoom <anjoompa42003@gmail.com>
2023-08-31 20:47:16 +0530

    splash screen text

---
+++ meal_app/lib/view/core/font.dart
@@ -0,0 +1,6 @@
+
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final gPrimaryFont = GoogleFonts.aBeeZee();
+final headingFont  = GoogleFonts.daysOne(fontWeight: FontWeight.w900);
--- meal_app/lib/view/splash_screen.dart
+++ meal_app/lib/view/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/view/core/font.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -7,7 +8,35 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Text("splash screen"),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              "assets/image/splash_image.jpg",
+              height: 400,
+            ),
+            Container(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "25K+ PREMIUM RECIPES",
+                      
+                      style: gPrimaryFont.copyWith(fontSize: 14,color: Colors.black54,letterSpacing: 1.5),
+                    ),
+                    SizedBox(height: 10,),
+                    Text(
+                      "it's\nCooking\nTime!",
+                      style: headingFont.copyWith(
+                        letterSpacing: 1.5,
+                        fontSize: 45
+                      ),
+                    ),
+                  ],
+                ))
+          ],
+        ),
       ),
     );
   }
