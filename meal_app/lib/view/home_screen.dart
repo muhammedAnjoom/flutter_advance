ac1f47aedecb6624a6b851d62caaf4a5c8cc62c8
muhammedAnjoom <anjoompa42003@gmail.com>
2023-09-04 21:41:30 +0530

    textfromfield decoration

--- meal_app/lib/view/home_screen.dart
+++ meal_app/lib/view/home_screen.dart
@@ -14,17 +14,43 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 10,),
+              const SizedBox(
+                height: 10,
+              ),
               SvgPicture.asset(
                 "assets/icons/menu.svg",
                 width: 25,
                 height: 25,
               ),
-              SizedBox(height: 20,),
+              const SizedBox(
+                height: 20,
+              ),
               Text(
                 "What would you\nlike ti cook?",
                 style: headingFont.copyWith(
                     fontSize: 28, fontWeight: FontWeight.w400),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      borderSide: BorderSide.none
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      borderSide: BorderSide.none
+                    ),
+                    fillColor: Color(0xffF3F3F3),
+                    focusColor: Colors.grey),
               )
             ],
           ),
