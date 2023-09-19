e14cd1dc402eec4fc8fc71e8c83209a57a0ee8d9
muhammedAnjoom <anjoompa42003@gmail.com>
2023-09-19 06:22:40 +0530

    add todo screen completed

--- todo_list/lib/view/add_todo.dart
+++ todo_list/lib/view/add_todo.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
+enum ActionType {
+  addTodo,
+  editTodo,
+}
+
 class AddToDo extends StatelessWidget {
   const AddToDo({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffFBF3E1),
       body: SafeArea(
         child: Container(
+          width: double.infinity,
+          height: double.infinity,
           child: Column(
             children: [
               Row(
@@ -20,12 +28,53 @@ class AddToDo extends StatelessWidget {
                         size: 28,
                       )),
                   TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        switch(ActionType){
+                          case ActionType.addTodo:
+                          //  addNote
+                            break;
+                          case ActionType.editTodo:
+                            //  edittodo
+                            break;
+                        }
+                      },
                       child: const Text(
                         "Done",
-                        style: TextStyle(fontSize: 22),
+                        style: TextStyle(
+                            fontSize: 22,
+                            color: Color(0xff8B6521),
+                            fontWeight: FontWeight.w400),
                       ))
                 ],
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: const Column(
+                    children: [
+                      TextField(
+                        style: TextStyle(
+                            fontSize: 30,
+                            color: Color(0xff8B6521),
+                            fontWeight: FontWeight.w800),
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintStyle: TextStyle(fontWeight: FontWeight.normal),
+                            hintText: "Enter Title"),
+                      ),
+                      Expanded(
+                        child: TextField(
+                          minLines: null,
+                          maxLines: null,
+                          expands: true,
+                          style: TextStyle(fontSize: 18),
+                          decoration:
+                              InputDecoration(hintText: "Enter decripiton"),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               )
             ],
           ),
