ce82564cc36013060f57fd2fb0e2608c1cba14e0
muhammedAnjoom <anjoompa42003@gmail.com>
2023-11-04 22:17:52 +0530

    handle api response

---
+++ getx_project/lib/data/response/api_response.dart
@@ -0,0 +1,20 @@
+import 'package:getx_project/data/response/status.dart';
+
+class ApiResponse<T> {
+  Status? status;
+  T? data;
+  String? message;
+
+  ApiResponse(this.status, this.data, this.message);
+
+  ApiResponse.loading() : status = Status.LOADING;
+  ApiResponse.completed() : status = Status.COMPLETED;
+
+  ApiResponse.eroor() : status = Status.ERROR;
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'Status : $status\n Message : $message \n Data : $data';
+  }
+}
