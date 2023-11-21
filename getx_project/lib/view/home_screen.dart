b9156637bab14e478c2438399507619e69cf99a4
muhammedAnjoom <anjoompa42003@gmail.com>
2023-11-21 22:03:17 +0530

    all user add in home page and handle error

--- getx_project/lib/view/home_screen.dart
+++ getx_project/lib/view/home_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/state_manager.dart';
+import 'package:getx_project/data/response/status.dart';
+import 'package:getx_project/res/components/general_expection.dart';
+import 'package:getx_project/res/components/internet_expection_widget.dart';
+import 'package:getx_project/res/routes/routes.dart';
 import 'package:getx_project/res/routes/routes_name.dart';
 import 'package:getx_project/view_models/controller/home/home_view_model.dart';
 import 'package:getx_project/view_models/controller/user_preferences/user_preference_view_model.dart';
@@ -13,16 +17,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final homeController = Get.put(HomeController());
 
-
- final homeController = Get.put(HomeController());
-
- @override
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
     homeController.userListApi();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,14 +35,50 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
         backgroundColor: Colors.blue,
         actions: [
-          IconButton(onPressed: (){
-            UserPrefences().removeUser().then((value){
-              Get.toNamed(RouteName.loginScreen);
-            });
-          }, icon: Icon(Icons.logout_outlined))
+          IconButton(
+              onPressed: () {
+                UserPrefences().removeUser().then((value) {
+                  Get.toNamed(RouteName.loginScreen);
+                });
+              },
+              icon: Icon(Icons.logout_outlined))
         ],
       ),
-      body: SafeArea(child: Text("hellow")),
+      body: Obx(() {
+        switch (homeController.rxRequestStatus.value) {
+          case Status.LOADING:
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          case Status.ERROR:
+            if (homeController.error.value == 'No internet') {
+              return Center(
+                child: InternetExpectionWidget(onPressed: () {
+                  homeController.refresheApi();
+                }),
+              );
+            } else {
+              return GeneralExpectionWidget(onPressed: (){
+                homeController.refresheApi();
+              });
+            }
+          case Status.COMPLETED:
+            return ListView.builder(
+              itemCount: homeController.userList.value.data!.length,
+              itemBuilder: (context, index) {
+                final item = homeController.userList.value.data![index];
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                        backgroundImage: NetworkImage(item.avatar.toString())),
+                    title: Text("${item.firstName} ${item.lastName}"),
+                    subtitle: Text(item.email.toString()),
+                  ),
+                );
+              },
+            );
+        }
+      }),
     );
   }
 }
--- getx_project/lib/view_models/controller/home/home_view_model.dart
+++ getx_project/lib/view_models/controller/home/home_view_model.dart
@@ -8,17 +8,28 @@ class HomeController extends GetxController {
 
   final rxRequestStatus = Status.LOADING.obs;
   final userList = UserListModel().obs;
-
+  RxString error = ''.obs;
   void setRxRequestStatus(Status _value) => rxRequestStatus.value = _value;
-    void setUserList(UserListModel _value) => userList.value = _value;
-
-
+  void setUserList(UserListModel _value) => userList.value = _value;
+  void setError(String _value) => error.value = _value;
   void userListApi() {
     _api.userList().then((value) {
       setRxRequestStatus(Status.COMPLETED);
       setUserList(value);
     }).onError((error, stackTrace) {
+      setError(error.toString());
+      setRxRequestStatus(Status.ERROR);
+    });
+  }
+  void refresheApi() {
+    setRxRequestStatus(Status.LOADING);
+    _api.userList().then((value) {
+      setRxRequestStatus(Status.COMPLETED);
+      setUserList(value);
+    }).onError((error, stackTrace) {
+      setError(error.toString());
       setRxRequestStatus(Status.ERROR);
     });
   }
 }
+
